73c02a83bf9388a893f87c3a6b6112f27d235c65
sanji2207 <saepullohepul565@gmail.com>
2023-05-18 06:58:00 +0700

    Semangat

---
+++ bin/belajar_dart_application.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_application/belajar_dart_application.dart' as belajar_dart_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_application.calculate()}!');
+}
